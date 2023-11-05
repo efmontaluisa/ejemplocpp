f6e7aa782e8985cb7c28234efea043df5144ac8b
Edgar Montaluisa <efmontaluisa73@gmail.com>
2023-11-05 18:58:09 -0500

    Tabla de Multiplicar

--- demo.cpp
+++ demo.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main()
-{   cout<<"Hola Mundo";
+{   cout<<"Hola Mundo"<<endl;
+    for(int i=0;i<=10;i++)
+    {
+        cout<<i<<" * "<<5<<" = " << i*5 <<endl;
+    }
     return 0;
 }
