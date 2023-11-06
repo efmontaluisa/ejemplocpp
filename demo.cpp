b2465e0909ad3318bf3736531eb6fdd5925be932
efmontaluisa <efmontaluisa73@gmail.com>
2023-11-05 20:28:14 -0500

    Final

--- demo.cpp
+++ demo.cpp
@@ -6,6 +6,11 @@ int main()
     for(int i=0;i<=10;i++)
     {
         cout<<i<<" * "<<5<<" = " << i*5 <<endl;
+        cout<<i<<" * "<<5<<" = " << i*5 <<endl;
+        cout<<i<<" * "<<5<<" = " << i*5 <<endl;
+        cout<<i<<" * "<<5<<" = " << i*5 <<endl;
+        cout<<i<<" * "<<5<<" = " << i*5 <<endl;
+
     }
     return 0;
 }
